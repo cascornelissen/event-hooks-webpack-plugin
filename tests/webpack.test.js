e3341930ae6ccb7a93f5a7995d2159a51d5689b6
Cas Cornelissen <cas.cornelissen@onefinity.io>
2020-11-02 19:03:32 +0100

    Extend testing setup to support testing both Webpack 4 and 5

--- tests/webpack.test.js
+++ tests/webpack.test.js
@@ -1,11 +1,10 @@
-import webpack from 'webpack';
 import 'regenerator-runtime/runtime';
 import EventHooksPlugin from '../lib/';
 import { CallbackTask, PromiseTask, Task } from '../lib/tasks';
 
 it('Throws a meaningful message when providing an invalid event hook name', () => {
     expect(() => {
-        webpack({
+        global.__WEBPACK__({
             plugins: [
                 new EventHooksPlugin({
                     abc: () => null
